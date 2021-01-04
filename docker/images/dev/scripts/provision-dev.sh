8356faa50fc75e29e64531a81a6a0b2d1341c1a0
Angel Manchev <angel@manchev.pro>
2021-01-04 15:52:41 +0200

    Add internal (entrypoint) tasks finally for dev container.

--- docker/images/dev/scripts/provision-dev.sh
+++ docker/images/dev/scripts/provision-dev.sh
@@ -9,14 +9,18 @@ export TERM=xterm
 
 . ${AYAQA_INFRA_LIBS_DIR}/liblog.sh
 
-info "Run provision for php"
+info "Run provision for: php"
 ansible-playbook --tags "debug_info" ${AYAQA_PROVISION_IMAGE_DIR}/php.yml
 ansible-playbook --tags "provision" ${AYAQA_PROVISION_IMAGE_DIR}/php.yml
 
-info "Run provision for phpfpm and nginx"
+info "Run provision for: phpfpm and nginx"
 ansible-playbook --tags "debug_info" ${AYAQA_PROVISION_IMAGE_DIR}/phpfpm-nginx.yml
 ansible-playbook --tags "provision" ${AYAQA_PROVISION_IMAGE_DIR}/phpfpm-nginx.yml
 
-info "Run provision for supervisor"
+info "Run provision for: supervisor"
 ansible-playbook --tags "debug_info" ${AYAQA_PROVISION_IMAGE_DIR}/supervisor.yml
 ansible-playbook ${AYAQA_PROVISION_IMAGE_DIR}/supervisor.yml
+
+info "Run internal provision"
+ansible-playbook --tags "debug_info" ${AYAQA_PROVISION_IMAGE_DIR}/internal.yml
+ansible-playbook --tags "provision"  ${AYAQA_PROVISION_IMAGE_DIR}/internal.yml
