6a08389ca552bea98c57cdf74f59670633e734cd
Angel Manchev <angel@manchev.pro>
2021-01-04 14:59:10 +0200

    Add provision for php,phpfpm,nginx and supervisor

--- docker/images/dev/scripts/provision-dev.sh
+++ docker/images/dev/scripts/provision-dev.sh
@@ -9,7 +9,14 @@ export TERM=xterm
 
 . ${AYAQA_INFRA_LIBS_DIR}/liblog.sh
 
-info "Run provision with ansible"
-# First will run it with debug_info to have all variables before run real provision
-ansible-playbook --tags "debug_info" ${AYAQA_PROVISION_IMAGE_DIR}/internal.yml
-ansible-playbook --tags "provision" ${AYAQA_PROVISION_IMAGE_DIR}/internal.yml
+info "Run provision for php"
+ansible-playbook --tags "debug_info" ${AYAQA_PROVISION_IMAGE_DIR}/php.yml
+ansible-playbook --tags "provision" ${AYAQA_PROVISION_IMAGE_DIR}/php.yml
+
+info "Run provision for phpfpm and nginx"
+ansible-playbook --tags "debug_info" ${AYAQA_PROVISION_IMAGE_DIR}/phpfpm-nginx.yml
+ansible-playbook --tags "provision" ${AYAQA_PROVISION_IMAGE_DIR}/phpfpm-nginx.yml
+
+info "Run provision for supervisor"
+ansible-playbook --tags "debug_info" ${AYAQA_PROVISION_IMAGE_DIR}/supervisor.yml
+ansible-playbook ${AYAQA_PROVISION_IMAGE_DIR}/supervisor.yml
