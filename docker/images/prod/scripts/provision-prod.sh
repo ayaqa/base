f3ea4e5c4151f4f669f0dd4b52a5185f40ddc0de
Angel Manchev <angel@manchev.pro>
2021-01-23 00:31:31 +0200

    Add raw build prod tasks and manifest.

--- docker/images/prod/scripts/provision-prod.sh
+++ docker/images/prod/scripts/provision-prod.sh
@@ -8,3 +8,19 @@ set -o pipefail
 export TERM=xterm
 
 . ${AYAQA_INFRA_LIBS_DIR}/liblog.sh
+
+info "Run git clone of dev repo."
+ansible-playbook --tags "debug_info" ${AYAQA_PROVISION_IMAGE_DIR}/clone.yml
+ansible-playbook --tags "provision" ${AYAQA_PROVISION_IMAGE_DIR}/clone.yml
+
+info "Run build and prepare app for production."
+ansible-playbook --tags "debug_info" ${AYAQA_PROVISION_IMAGE_DIR}/build.yml
+ansible-playbook --tags "provision" ${AYAQA_PROVISION_IMAGE_DIR}/build.yml
+
+info "Call all provision files to clear everything that is not needed for prod build."
+shopt -s globstar
+for i in ${AYAQA_INFRA_PROVISION_DIR}/*/*.yml; do
+    info "Call playbook: ${i}"
+    ansible-playbook --tags "clean" "${i}"
+done
+shopt +s globstar
