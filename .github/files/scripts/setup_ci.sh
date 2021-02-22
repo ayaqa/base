e08b810804738b606ae6f0ea68ac3c58f2e8a79f
Angel Manchev <angel@manchev.pro>
2021-02-23 00:24:34 +0200

    Refactor github actions on push on main

--- .github/files/scripts/setup_ci.sh
+++ .github/files/scripts/setup_ci.sh
@@ -2,4 +2,9 @@
 
 set -x
 
-cp .github/files/config-ci.json ./config-local.json
+CONFIG_LOCAL_PATH="config-local.json"
+CONFIG_CI_PATH=${1:-"config-push-main.json"}
+
+echo "Config file: ${CONFIG_CI_PATH}"
+
+cp .github/files/${CONFIG_CI_PATH} ./${CONFIG_LOCAL_PATH}
